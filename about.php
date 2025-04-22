b193c94dde38bda5b2b48d75376d2bc305704800
InDaBottleLee <leejosephcaceres@gmail.com>
2025-04-22 08:59:05 +0800

    about both php and css

--- about.php
+++ about.php
@@ -23,20 +23,36 @@
 
             <img id='logo' src="icon.png" alt="logo.png">
 
-            <a href="index.php"><button id='home'>Home</button></a>
-            <a href="about.php"><button id='about'>About</button></a>
-            <a href="terms.php"><button id='terms'>Terms</button></a>
-            <a href="create.php"><button id='create'>Create</button></a>
+
 
         </div>
 
         <div id='right-nav'>
-            
+            <a href="index.php"><button id='home'>Home</button></a>
+            <a href="about.php"><button id='about'>About</button></a>
+            <a href="terms.php"><button id='terms'>Terms</button></a>
+            <a href="create.php"><button id='create'>Create</button></a>
             <a href="signup.php"><img src="profile.png" id='profile' alt="profile.png"></a>
+
 
         </div>
 
     </div>
+
+
+    <div id="desc">
+        <p>
+            <h1>LET THE WHISPERS<br>
+            <center>BE DELIVERED.</center></h1>
+            
+            Deliver the words you were 
+            never meant to say. One note at a time.
+        </p>
+    </div>
+
+    <div id="panel1">
+        
+    </div>   
     
 </body>
 
