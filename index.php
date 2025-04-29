e5881fe8f6840b2d1aa36d1cbb31384ba5652c8c
InDaBottleLee <leejosephcaceres@gmail.com>
2025-04-30 03:24:29 +0800

    Home Page Updates

--- index.php
+++ index.php
@@ -40,6 +40,44 @@
         <a href="create.php"><button><h2>Deliver a Message</h2></button></a>
         <img id='woman' src="woman.png" alt="woman.png">
     </div>
+
+    <div id='title-container'>
+        <h1>Here is how Delivered works</h1>
+        
+    </div>
+
+    <div id ='onetwothree'>
+        <div id='one'>
+        <span class="dot">
+            <h3>1</h3>
+        </span>
+        <p>Sign in to your
+        account.</p>
+        </div>
+
+        <div id='two'>
+        <span class="dot">
+            <h3>2</h3>
+        </span>
+        <p>Create your message
+        to be delivered</p>
+        </div>
+
+        
+        <div id='three'>
+        <span class="dot">
+            <h3>3</h3>
+        </span>
+        <p>Submit the note and wait for it to be delivered. </p>
+        </div>
+       
+        
+
+
+
+
+
+    </div>   
     <div id='footer-container'>
         <div>
             <h2>[Delivered]</h2>
